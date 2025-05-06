22176ac4f4ae7b04148a05450ffbd96890d7efd0
unknown <SSDD@PNAWSA05LC52203.duoc.acad>
2025-05-06 14:49:28 -0400

    cambio1

---
+++ src/test/java/com/example/primer/proyecto/PrimerProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primer.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
